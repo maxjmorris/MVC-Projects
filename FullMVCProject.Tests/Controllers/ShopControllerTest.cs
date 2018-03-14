d4f11485f9777c648436c1ba803a995d5aa50c1d
p15213515 <p15213515@KLIXX104WIN076W>
2018-03-14 13:28:45 +0000

    Add project files.

---
+++ FullMVCProject.Tests/Controllers/ShopControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using FullMVCProject.Controllers;
+namespace FullMVCProject.Controllers
+{
+    [TestClass]
+    public class ShopControllerTest
+    {
+        [TestMethod]
+        public void IndexWelcomeMessage()
+        {
+
+            //Arrange
+
+            ShopController
+        }
+    }
+}
